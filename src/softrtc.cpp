2378279d263e379fd49e2beb2755de571416a643
Bill Siever <bsiever@gmail.com>
2020-06-05 12:19:52 -0500

    Added template block definitions.

--- src/softrtc.cpp
+++ src/softrtc.cpp
@@ -4,6 +4,7 @@
 #include "MicroBitSystemTimer.h"
 
 
+
 // Static variable(s) for "zero time"
 static time_t timeAtSetpoint = 0;
 static uint64_t setpoint = 0; 
@@ -47,7 +48,6 @@ static time_t systemTime() {
 void periodicUpdate() {
     while(true) {
         systemTime();
-//        fiber_sleep(250);  // Sleep 0.25s seconds
         fiber_sleep(5*60*1000);  // Sleep 5 minutes seconds
     }
 }
@@ -178,3 +178,137 @@ int getRTCSRC() {
 uint32_t getTicker() {
     return us_ticker_read();
 }
+
+/*
+Blocks initial prototypes: 
+
+
+
+class DateTime {
+
+}
+
+enum MornNight {
+    //% block="am"
+    AM,
+    //% block="pm"
+    PM
+}
+
+enum TimeUnit {
+    //% block="ms"
+    Milliseconds,
+    //% block="Seconds"
+    Seconds, 
+    //% block="Minutes"
+    Minutes, 
+    //% block="Hours"
+    Hours, 
+    //% block="Days"
+    Days
+}
+
+enum TimeFormat {
+    //% block="omit"
+    None,
+    //% block="am / pm"
+    AMPM,
+    //% block="24-hr"
+    HHMM24hr
+}
+
+enum DateFormat {
+    //% block="omit"
+    None,
+    //% block="Day/Month"
+    DM,
+    //% block="Day/Month/Year"
+    DMYYYY,
+    //% block="YEAR-MONTH-DAY"
+    YYYY_MM_DD
+}
+
+// Fontawesome Unicode maybe: "&#xf017;"
+//% color="#AA278D"  icon="\u23f0" 
+namespace timeAndDate {
+
+
+    //% block="set 24-hour time to |  %hour | : %minute | . %second"
+    //% hour.min=0 hour.max=23
+    //% minute.min=0 minute.max=59
+    //% second.min=0 second.max=59
+    export function set24HourTime(hour: number, minute: number, second: number) {
+
+    }
+    //% block="set time to |  %hour | : %minute | . %second | %ampm"
+    //% hour.min=0 hour.max=23
+    //% minute.min=0 minute.max=59
+    //% second.min=0 second.max=59
+    //% inlineInputMode=inline
+    export function setTime(hour: number, minute: number, second: number, ampm: MornNight) {
+
+    }
+
+
+    //% block="set date to | Day %day | Month %month | Year %year"
+    //% day.min=0 day.max=31
+    //% month.min=1 month.max=12
+    //% year.min=2020 year.max=2050
+    export function setDate(day: number, month: number, year: number) {
+
+    }
+
+    //% block="time in format %tformat | date in format: %dformat "
+    export function timeAndDate(tformat: TimeFormat, dformat: DateFormat) : String {
+       return ""
+    }
+
+    // Format:  "YYYY-MM-DD HH:MM" in 24-hour format
+    //% block="time stamp"
+    export function timestamp() : String {
+       return ""
+    }
+
+
+    // This can cause overflow or underflow (adding 1 minute could change the hour)
+    // Add or subtract time with the given unit. 
+    //% block="advance time/date by | %amount | %unit "
+    export function advanceBy(amount: number, unit: TimeUnit) {
+
+    }
+
+
+    //% block="current time $hour:$minute.$second on $weekday, $day/$month/$year" advanced=true
+    //% instance.shadow=variables_get
+    //% instance.defl=time
+    //% draggableParameters=variable
+    //% handlerStatement=1
+    export function exampleHandler(instance: DateTime, handler: (hour: number, minute: number, second: number, weekday: string, day: number, month: number, year: number) => void) {
+
+    }
+
+
+    //% block="minute changed" advanced=true
+    export function onMinuteChanged(handler: () => void) {
+
+    }
+
+    //% block="second changed" advanced=true
+    export function onSecondChanged(handler: () => void) {
+
+    }
+    //% block="hour changed" advanced=true
+    export function onHourChanged(handler: () => void) {
+
+    }
+    //% block="day changed" advanced=true
+    export function onDayChanged(handler: () => void) {
+
+    }
+
+
+    //  Advanced: 
+    
+    // Get time;  time->day/month/year/hours(24)/hours/amPM string / minutes/seconds/weekday/ daystring w/format  / year string w/format
+}
+*/
